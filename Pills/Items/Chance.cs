4deef3e8e90146e7efcc4c426abc24a3a186bd83
TagGodOnYT <tristan@schreck84.com>
2024-03-03 23:39:08 -0600

    no

--- Pills/Items/Chance.cs
+++ Pills/Items/Chance.cs
@@ -5,6 +5,10 @@
         public int MinimumChance { get; set; }
         public int MaximumChance { get; set; }
 
+        public Chance()
+        {
+        }
+
         public Chance(int minimumChance, int maximumChance)
         {
             MinimumChance = minimumChance;
